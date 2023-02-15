529525c918f414ac0f2099907ce94b4273d6e5ed
Abhinash Bhengra <abhinashbhengra170dx@gmail.com>
2023-02-15 13:07:29 +0530

    react-vite initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import About from "./About";
+import Contact from "./Contact";
+import Home from "./Home";
+import Service from "./Service";
+
+const App = () => {
+  return (
+    <div>
+      <Home />
+      <About />
+      <Service />
+      <Contact />
+    </div>
+  );
+};
+
+export default App;
