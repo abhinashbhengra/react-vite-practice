f7db02896ad4d73eae3a937311da4bd2d5666c74
Abhinash Bhengra <abhinashbhengra170dx@gmail.com>
2023-02-17 16:29:00 +0530

    Hero section update

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -6,7 +6,7 @@ import { Button } from "./ButtonStyle";
 const HeroSection = () => {
   return (
     <Wrapper>
-      <div className="container grid grid-two-colum">
+      <div className="container grid grid-two-column">
         <div className="hero-section-data">
           <p className="hero-top-data">Hi 👋, I'm</p>
           <h1 className="hero-heading">Abhinash</h1>
@@ -17,12 +17,55 @@ const HeroSection = () => {
             <NavLink to="contact">Hire Me</NavLink>
           </Button>
         </div>
-        <div className="hero-section-image"></div>
+        <div className="hero-section-image">
+          <picture>
+            <img className="hero-img" src="./images/hero.svg" alt="hero" />
+          </picture>
+        </div>
       </div>
     </Wrapper>
   );
 };
 
-const Wrapper = styled.section``;
+const Wrapper = styled.section`
+  /* border: 2px solid red; */
+  padding: 9rem 0;
+  .hero-section-data {
+    /* border: 2px solid green; */
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+
+    .hero-top-data {
+      text-transform: uppercase;
+      font-size: 1.5rem;
+      font-weight: 500;
+      color: ${({ theme }) => theme.colors.helper};
+    }
+
+    .hero-heading {
+      text-transform: uppercase;
+      font-size: 6.4rem;
+    }
+    .hero-para {
+      margin-top: 1.5rem;
+      margin-bottom: 3.4rem;
+      max-width: 60rem;
+    }
+
+    .btn {
+      max-width: 16rem;
+    }
+  }
+
+  picture {
+    /* border: 2px solid violet; */
+    text-align: center;
+
+    .hero-img {
+      max-width: 80%;
+    }
+  }
+`;
 
 export default HeroSection;
