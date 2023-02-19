e7e201f244c2d57afe2b7c0077945155ccf8345c
Abhinash Bhengra <abhinashbhengra170dx@gmail.com>
2023-02-19 18:32:03 +0530

    context update

--- src/context.jsx
+++ src/context.jsx
@@ -1,11 +1,39 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useReducer } from "react";
+import { reducer } from "../src/reducer";
 
 const AppContext = createContext(); // create context
 
+const initialState = {
+  name: "",
+  image: "",
+  role: "",
+};
 const AppProvider = ({ children }) => {
   // provider
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const updateHomePage = () => {
+    return dispatch({
+      type: "HOME_UPDATE",
+      payload: {
+        name: "Abhinash",
+        image: "./images/hero.svg",
+        role: "frontend developer",
+      },
+    });
+  };
+  const updateAboutPage = () => {
+    return dispatch({
+      type: "ABOUT_UPDATE",
+      payload: {
+        name: "Abhinash Bhengra",
+        image: "./images/about1.svg",
+        role: "frontend developer",
+      },
+    });
+  };
+
   return (
-    <AppContext.Provider value="frontend developer">
+    <AppContext.Provider value={{ ...state, updateHomePage, updateAboutPage }}>
       {children}
     </AppContext.Provider>
   );
