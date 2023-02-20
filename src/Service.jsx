8911c77c2ebb55f4d51936508410957bca27b10f
Abhinash Bhengra <abhinashbhengra170dx@gmail.com>
2023-02-20 23:03:59 +0530

    component update

--- src/Service.jsx
+++ src/Service.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import styled from "styled-components";
+import { useGlobalContext } from "./context";
 
 const Service = () => {
-  return <div>Service</div>;
+  const { services } = useGlobalContext();
+  console.log(services);
+  return (
+    <Wrapper className="section">
+      <h2 className="common-heading">Our Services</h2>
+      <div className="container grid grid-three-column">
+        {services.map((service) => {
+          const { id, name } = service;
+          return (
+            <div key={id} className="card">
+              <p>{name}</p>
+            </div>
+          );
+        })}
+      </div>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.section``;
 
 export default Service;
