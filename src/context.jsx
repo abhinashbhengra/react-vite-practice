5cf7785466c60090c5b2688b5b8a0333697ab0fc
Abhinash Bhengra <abhinashbhengra170dx@gmail.com>
2023-02-20 23:02:50 +0530

    api function added

--- src/context.jsx
+++ src/context.jsx
@@ -1,12 +1,16 @@
-import { createContext, useContext, useReducer } from "react";
+import { createContext, useContext, useEffect, useReducer } from "react";
 import { reducer } from "../src/reducer";
 
 const AppContext = createContext(); // create context
+
+const API =
+  "https://e86889d7-f36f-4c07-90a0-2d87576cf6a5.mock.pstmn.io/getServices";
 
 const initialState = {
   name: "",
   image: "",
   role: "",
+  services: [],
 };
 const AppProvider = ({ children }) => {
   // provider
@@ -31,6 +35,20 @@ const AppProvider = ({ children }) => {
       },
     });
   };
+  //api data
+  const getServices = async (url) => {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      dispatch({ type: "GET_SERVICES", payload: data });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  // api call
+  useEffect(() => {
+    getServices(API);
+  }, []);
 
   return (
     <AppContext.Provider value={{ ...state, updateHomePage, updateAboutPage }}>
