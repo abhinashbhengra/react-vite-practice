ea336b0d8f40cbd3a3b2b79d916d09aa39e306ed
Abhinash Bhengra <abhinashbhengra170dx@gmail.com>
2023-02-22 14:59:30 +0530

    component update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,130 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
+import { Button } from "./ButtonStyle";
+import { FaDiscord, FaInstagram, FaYoutube } from "react-icons/fa";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <Wrapper>
+      <section className="contact-short">
+        <div className="grid grid-two-column">
+          <div>
+            <h3>Ready to get started?</h3>
+            <h3>Talk to us today</h3>
+          </div>
+          <div className="contact-short-btn">
+            <NavLink>
+              <Button>Get Started</Button>
+            </NavLink>
+          </div>
+        </div>
+      </section>
+
+      <footer>
+        <div className="container grid grid-four-column">
+          <div className="footer-about">
+            <h3>Abhinash Bhengra</h3>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+          </div>
+
+          <div className="footer-subscribe">
+            <h3>Subscribe to get important updates</h3>
+            <form action="#">
+              <input
+                type="email"
+                required
+                autoComplete="off"
+                placeholder="Email"
+              />
+              <input type="submit" value="Subscribe" />
+            </form>
+          </div>
+
+          <div className="footer-social">
+            <h3>Follow us</h3>
+            <div className="footer-social--icons">
+              <div>
+                <FaDiscord className="icons" />
+              </div>
+              <div>
+                <FaInstagram className="icons" />
+              </div>
+              <div>
+                <FaYoutube className="icons" />
+              </div>
+            </div>
+          </div>
+
+          <div className="footer-contact">
+            <h3>Call me</h3>
+            <h3>+92 748947309</h3>
+          </div>
+        </div>
+
+        <div className="footer-bottom--section">
+          <hr />
+          <div className="container grid grid-two-column">
+            <p> @{new Date().getFullYear()} Abhinash. All Rights Reserved</p>
+            <div>
+              <p>Piracy Policy</p>
+              <p>Terms and Conditions</p>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.section`
+  .contact-short {
+    max-width: 60vw;
+    margin: auto;
+    padding: 5rem 10rem;
+    background-color: ${({ theme }) => theme.colors.bg};
+    border-radius: 1rem;
+    box-shadow: ${({ theme }) => theme.colors.shadowSupport};
+    transform: translateY(50%);
+  }
+  .contact-short-btn {
+    justify-self: end;
+    align-self: center;
+  }
+  footer {
+    padding: 14rem 0 9rem 0;
+    background-color: ${({ theme }) => theme.colors.footer_bg};
+    h3 {
+      color: ${({ theme }) => theme.colors.hr};
+      margin-bottom: 2.4rem;
+    }
+    p {
+      color: ${({ theme }) => theme.colors.white};
+    }
+    .footer-social--icons {
+      display: flex;
+      gap: 2rem;
+      div {
+        padding: 1rem;
+        border-radius: 50%;
+        border: 2px solid ${({ theme }) => theme.colors.white};
+        .icons {
+          color: ${({ theme }) => theme.colors.white};
+          font-size: 2.4rem;
+          position: relative;
+          cursor: pointer;
+        }
+      }
+    }
+    .footer-bottom--section {
+      padding-top: 9rem;
+      hr {
+        margin-bottom: 2rem;
+        color: ${({ theme }) => theme.colors.hr};
+        height: 0.1px;
+      }
+    }
+  }
+`;
 
 export default Footer;
